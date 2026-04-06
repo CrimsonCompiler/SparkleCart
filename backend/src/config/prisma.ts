447f79de49bc65feea0144636019bed0e8e47321
CrimsonCompiler <toisiftasrik@gmail.com>
2026-04-06 18:18:08 +0600

    feat:  initialize Prisma client with PostgreSQL adapter

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,10 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config({ quiet: true });
 import { PrismaClient } from "../../generated/prisma";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+// 1. Database er sathe connection pool
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+// 2. Prisma er jonno adapter
+const adapter = new PrismaPg(pool);
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient({});
+const prisma = global.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
