9ca302facb80d5e74814fc99cb2e92b823f8405e
CrimsonCompiler <toisiftasrik@gmail.com>
2026-04-07 00:44:11 +0600

    update:  added dotenv package and dotenv config . fixed String to string

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
-
+import dotenv from "dotenv";
+dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET || "aquafina_water_bottle_ekhane";
 
-export const generateToken = (userId: String, role: String) => {
-  return jwt.sign({ userId, role }, JWT_SECRET, { expiresIn: "7d" });
+export const generateToken = (id: string, role: string) => {
+  return jwt.sign({ id, role }, JWT_SECRET, { expiresIn: "7d" });
 };
