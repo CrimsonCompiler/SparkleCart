d2baff9f40eaa870777ad3e33c69cfe97f1bc102
CrimsonCompiler <toisiftasrik@gmail.com>
2026-04-06 10:35:25 +0600

    feat: made basic customer registration route . Added main router to gather all the routes and used it on the server file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import mainRouter from "./routes/index";
 dotenv.config();
 
 const app: Application = express();
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", mainRouter);
 
 app.get("/health", (req: Request, res: Response) => {
   res.status(200).json({ status: "SparkleCart API is running smoothly.." });
