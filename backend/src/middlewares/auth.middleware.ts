96cdcfbd4037a2ff95e3efa4b96b5f6bef712df0
CrimsonCompiler <toisiftasrik@gmail.com>
2026-04-07 00:42:35 +0600

    update: changed the JWT SECRET key

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET || "__876@#sparklecart__00";
+const JWT_SECRET = process.env.JWT_SECRET || "aquafina_water_bottle_ekhane";
 
 export interface DecodedToken {
   id: string;
