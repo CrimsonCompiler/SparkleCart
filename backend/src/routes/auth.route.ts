ef48e4d064088e3ad4acd799174385e773985d86
CrimsonCompiler <toisiftasrik@gmail.com>
2026-04-06 19:51:47 +0600

    feat: add authentication routes for customer registration and login

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { customerRegister } from "../controllers/auth.controller";
+import {
+  customerLogin,
+  customerRegister,
+} from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post("/register", customerRegister);
+router.post("/login", customerLogin);
 
 export default router;
