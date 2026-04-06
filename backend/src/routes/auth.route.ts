0017d75b252266d87ffdc36470edbec093cad3ec
CrimsonCompiler <toisiftasrik@gmail.com>
2026-04-07 00:43:08 +0600

    update: added route for the customer profile dummy endpoint

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import {
   customerLogin,
   customerRegister,
+  getCustomerProfile,
 } from "../controllers/auth.controller";
+import { protect } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.post("/register", customerRegister);
 router.post("/login", customerLogin);
+
+router.get("/me", protect, getCustomerProfile);
 
 export default router;
