b5105e91634bf1f8f6321a0b3588be9ca9f59abc
CrimsonCompiler <toisiftasrik@gmail.com>
2026-04-07 11:57:06 +0600

    update: Getting all the products using all-products

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -42,3 +42,28 @@ export const createProduct = async (
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const getAllProducts = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const products = await prisma.product.findMany({
+      include: {
+        variants: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    res.status(200).json({
+      message: "Products fetched successfully",
+      count: products.length,
+      products,
+    });
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/src/routes/product.route.ts
+++ backend/src/routes/product.route.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createProduct } from "../controllers/product.controller";
+import {
+  createProduct,
+  getAllProducts,
+} from "../controllers/product.controller";
 import { protect } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.post("/create", protect, createProduct);
+router.get("/all-products", getAllProducts);
 
 export default router;
