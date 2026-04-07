d464efd989cf232c0df3e1fd03d5e62ab932e44e
CrimsonCompiler <toisiftasrik@gmail.com>
2026-04-07 10:55:35 +0600

    update: changed the name of the product schema function

--- backend/src/schemas/product.schema.ts
+++ backend/src/schemas/product.schema.ts
@@ -7,7 +7,7 @@ const variantSchema = z.object({
   sku: z.string().min(1, "SKU is required"),
 });
 
-export const productSchema = z.object({
+export const createProductSchema = z.object({
   title: z.string().min(3, "Title must be at least 3 characters"),
   description: z.string().min(10, "Description mus be at least 10 characters"),
   basePrice: z.number().positive("Price must be greater than 0"),
