0e514f809f16404bb7517db8c880a7cc356903e0
CrimsonCompiler <toisiftasrik@gmail.com>
2026-04-07 12:26:05 +0600

    feat: implemented product retrieval using Id

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -67,3 +67,32 @@ export const getAllProducts = async (
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const getProductById = async (
+  req: Request<{ id: string }>,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+
+    const product = await prisma.product.findUnique({
+      where: { id },
+      include: {
+        variants: true,
+      },
+    });
+
+    if (!product) {
+      res.status(404).json({ message: "Product not found" });
+      return;
+    }
+
+    res.status(200).json({
+      message: "Product fetched successfully",
+      product,
+    });
+  } catch (error) {
+    console.error("Error fetching single product:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/src/routes/product.route.ts
+++ backend/src/routes/product.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   createProduct,
   getAllProducts,
+  getProductById,
 } from "../controllers/product.controller";
 import { adminOnly, protect } from "../middlewares/auth.middleware";
 
@@ -9,5 +10,6 @@ const router = Router();
 
 router.post("/create", protect, adminOnly, createProduct);
 router.get("/all-products", getAllProducts);
+router.get("/:id", getProductById);
 
 export default router;
